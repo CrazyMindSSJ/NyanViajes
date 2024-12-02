d9d3f41a5f9779a80c396d9299df0620a10efc9a
crazymindssj <seba.urrutia@duocuc.cl>
2024-12-02 01:07:23 -0300

    profe tuvimos problemas de ultimo momento perqa se soluciono

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyDJhCs7qxt5TSDZhDM0GWmpoYWJUagw-_c",
-    authDomain: "nyanviajes.firebaseapp.com",
-    projectId: "nyanviajes",
-    storageBucket: "nyanviajes.firebasestorage.app",
-    messagingSenderId: "888266388751",
-    appId: "1:888266388751:web:7fa50b118f4a72518f7a7a"
+    apiKey: "AIzaSyDHi_G0N-wFIeT2K89mCeF4ytLeipbOkGw",
+    authDomain: "nyanviajes-e44af.firebaseapp.com",
+    projectId: "nyanviajes-e44af",
+    storageBucket: "nyanviajes-e44af.firebasestorage.app",
+    messagingSenderId: "215032184134",
+    appId: "1:215032184134:web:5341a094455c248385f947",
+    measurementId: "G-HLX6YCQRJ3"
   }
 };
 
